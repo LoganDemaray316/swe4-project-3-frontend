ab00bf52607eaa7eee4347841ffbdf2a74a0102f
Nathan Lalli <nathan.lalli@eagles.oc.edu>
2022-10-24 11:03:16 -0500

    Add the authentication

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
+import VueRouter from "vue-router";
+import router from "./router";
+import store from "./store/store";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
@@ -25,9 +29,12 @@ library.add(faTrash);
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
+Vue.use(VueRouter);
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
+  store,
   vuetify,
   render: (h) => h(App),
 }).$mount("#app");
