8eddad18bd919eb456249b6021a0df3c018506a1
Nathan Lalli <nathan.lalli@eagles.oc.edu>
2022-10-24 23:11:06 -0500

    Adds the rest of the services to the frontend

--- src/services/buildings.js
+++ src/services/buildings.js
@@ -6,5 +6,14 @@ class BuildingsDataService {
   update(id, data) {
     return http.post(`/schedule-t3/buildings/${id}`, data);
   }
+  getAll() {
+    return http.get("/schedule-t3/buildings");
+  }
+  getName(name) {
+    return http.get(`/schedule-t3/name/${name}`);
+  }
+  delete(id) {
+    return http.delete(`/schedule-t3/buildings/${id}`);
+  }
 }
 export default new BuildingsDataService();
