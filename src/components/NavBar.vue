7980afcddcd016209796e715cd9b73554c22e62b
Logan Demaray <l.demaray@eagles.oc.edu>
2022-10-26 09:29:42 -0500

    Correctly placed buttons

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,20 +1,32 @@
 <template>
-  <v-app-bar app color="darkBlue" dark>
+  <v-toolbar app color="darkBlue" dark>
     <v-img
       alt="Vuetify Logo"
       class="shrink mr-4"
       contain
       src="../assets/OC_LOGO_WHITE.svg"
       transition="scale-transition"
-      width="40" />
+      width="40"
+    />
 
     <v-toolbar-title class="font-weight-bold">SECTION PLANNING</v-toolbar-title>
-  </v-app-bar>
+    <v-spacer></v-spacer>
+    <v-toolbar-items>
+      <v-btn depressed color="darkBlue" class="font-weight-bold"
+        >Dashboard</v-btn
+      >
+      <v-btn depressed color="darkBlue" class="font-weight-bold"
+        >Course Catalog</v-btn
+      >
+    </v-toolbar-items>
+  </v-toolbar>
 </template>
 
 <script>
-  export default {
-    name: "NavBar",
-    data: () => ({}),
-  };
+export default {
+  name: "NavBar",
+  data: () => ({}),
+};
 </script>
+
+<style></style>
