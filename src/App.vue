4657d2e52af838e5c24fa5b70f19f9e9e7368b0f
Peyton Lalli <peyton.lalli@eagles.oc.edu>
2022-11-03 16:24:30 -0500

    First commit

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,20 @@
     <NavBar></NavBar>
 
     <v-main>
-      <v-container></v-container>
+      <SectionPlanner> </SectionPlanner>
     </v-main>
   </v-app>
 </template>
 
 <script>
   import NavBar from "./components/NavBar.vue";
+  import SectionPlanner from "./components/SectionPlanner.vue";
 
   export default {
     name: "App",
     components: {
       NavBar,
+      SectionPlanner
     },
     data: () => ({}),
   };
