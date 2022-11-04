fd722ef60ef3d1127d318a04c5d56d6f07427a3a
Logan Demaray <l.demaray@eagles.oc.edu>
2022-11-04 10:15:55 -0500

    Added sidebar and navbar section and can change between each

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,40 @@
 <template>
   <v-app>
-    <NavBar></NavBar>
+    <v-navigation-drawer v-model="sidebar" app>
+      <v-list>
+        <v-list-item
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.path">
+          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="darkBlue" dark>
+      <span class="hidden-md-and-up">
+        <v-app-bar-nav-icon @click="sidebar = !sidebar"></v-app-bar-nav-icon>
+      </span>
+      <v-img
+        alt="OC Logo"
+        class="shrink mr-4"
+        contain
+        src="./assets/OC_LOGO_WHITE.svg"
+        transition="scale-transition"
+        width="40" />
+
+      <v-toolbar-title class="font-weight-bold">DASHBOARD</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn
+          depressed
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.path">
+          {{ item.title }}
+        </v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
 
     <v-main class="lightGray">
       <HomeDashboard></HomeDashboard>
@@ -9,16 +43,16 @@
 </template>
 
 <script>
-  import NavBar from "./components/NavBar.vue";
-  import HomeDashboard from "./components/HomeDashboard.vue";
-
   export default {
     name: "App",
-    components: {
-      NavBar,
-      HomeDashboard,
-    },
-    data: () => ({}),
+    data: () => ({
+      sidebar: false,
+      menuItems: [
+        { title: "Dashboard", path: "/" },
+        { title: "Section Planner", path: "/" },
+        { title: "Course Catelog", path: "/" },
+      ],
+    }),
     computed: {
       theme() {
         return this.$vuetify.theme.dark ? "dark" : "light";
