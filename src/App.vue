64984d9effa4e30115c296e43086b13b5a9e44da
ethanimooney <ethanimooney@gmail.com>
2022-11-07 23:41:30 -0600

    Adds dashboard and section item elements

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,27 @@
   <v-app>
     <NavBar></NavBar>
 
-    <v-main>
-      <v-container></v-container>
+    <v-main class="lightGray">
+      <HomeDashboard></HomeDashboard>
     </v-main>
   </v-app>
 </template>
 
 <script>
   import NavBar from "./components/NavBar.vue";
+  import HomeDashboard from "./components/HomeDashboard.vue";
 
   export default {
     name: "App",
     components: {
       NavBar,
+      HomeDashboard,
     },
     data: () => ({}),
+    computed: {
+      theme() {
+        return this.$vuetify.theme.dark ? "dark" : "light";
+      },
+    },
   };
 </script>
---
+++ src/components/HomeDashboard.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12">
+        <v-card>
+          <v-card-title class="font-weight-bold white--text darkBlue">
+            YOUR SECTIONS
+          </v-card-title>
+          <v-container fluid>
+            <v-row dense>
+              <v-col
+                v-for="section in sections"
+                :key="section.sectionId"
+                :lg="3"
+                :md="6"
+                :sm="6"
+                :xs="12">
+                <SectionItem :section="section"></SectionItem>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card>
+      </v-col>
+      <v-col cols="6">
+        <v-card>
+          <v-card-title class="font-weight-bold white--text darkBlue">
+            FAVORITES
+          </v-card-title>
+          <v-card-text> </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="6">
+        <v-card>
+          <v-card-title class="font-weight-bold white--text darkBlue">
+            ABOUT
+          </v-card-title>
+          <v-card-text> </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  import SectionItem from "./SectionItem.vue";
+
+  export default {
+    name: "HomeDashboard",
+    components: {
+      SectionItem,
+    },
+    data() {
+      return {
+        // Fake section data that will be replaced by a store getter to fill this list with section objects.
+        sections: [
+          {
+            sectionId: 0,
+            sectionNumber: "CMSC-2113",
+            sectionInstanceNumber: 1,
+            sectionClass: "Object Oriented Programming",
+            sectionStartTime: "10:00am",
+            sectionEndTime: "10:55am",
+            sectionDOW: ["M", "W", "F"],
+            sectionLocation: "PEC 213",
+            sectionSize: 30,
+            sectionDate: "08/06/2022 - 11/18/2022",
+            sectionSemester: "Fall",
+            sectionTerms: ["T1", "T2"],
+          },
+          {
+            sectionId: 1,
+            sectionNumber: "CMSC-2413",
+            sectionInstanceNumber: 1,
+            sectionClass: "Assembly Language",
+            sectionStartTime: "11:00am",
+            sectionEndTime: "11:55am",
+            sectionDOW: ["T", "TH"],
+            sectionLocation: "PEC 211",
+            sectionSize: 30,
+            sectionDate: "08/06/2022 - 11/18/2022",
+            sectionSemester: "Fall",
+            sectionTerms: ["T1", "T2"],
+          },
+        ],
+      };
+    },
+  };
+</script>
