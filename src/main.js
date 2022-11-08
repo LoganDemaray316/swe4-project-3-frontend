31107e37131380ee4e251c41dabd20c5104d20b4
ethanimooney <ethanimooney@gmail.com>
2022-11-07 23:41:30 -0600

    Adds dashboard and section item elements

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import vuetify from "./plugins/vuetify";
 import VueRouter from "vue-router";
 import router from "./router";
 import store from "./store/store";
-//import "material-design-icons-iconfont/dist/material-design-icons.css";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
@@ -30,6 +29,30 @@ library.add(faTrash);
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.use(VueRouter);
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { faUserGroup } from "@fortawesome/free-solid-svg-icons";
+import { faCalendar } from "@fortawesome/free-solid-svg-icons";
+import { faCalendarWeek } from "@fortawesome/free-solid-svg-icons";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
+import { faCalendarDays } from "@fortawesome/free-solid-svg-icons";
+import { faPen } from "@fortawesome/free-solid-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faLocationDot);
+library.add(faUserGroup);
+library.add(faCalendar);
+library.add(faCalendarWeek);
+library.add(faClock);
+library.add(faCalendarDays);
+library.add(faPen);
+library.add(faTrash);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
 Vue.config.productionTip = false;
 
 new Vue({
